d7d71976acf9707f4ca5a8b192611d792f8220ae
jeffrgh <hjeff2507@gmail.com>
2025-11-11 20:38:39 +0530

    Slightly modified jump textures

--- src/player.cpp
+++ src/player.cpp
@@ -8,7 +8,6 @@ Player::Player()
     animFrame = 0;
     animTimer = 0.f;
     currentFrameCount = IDLE_FRAMES;
-    
 }
 
 // This function loads all the textures the player needs
@@ -26,7 +25,7 @@ void Player::loadAssets()
     {
         std::cerr << "Error loading Jump.png (Using Idle as fallback)" << std::endl;
         // Fallback: If Jump.png is missing, just use Idle.png
-        jumpTexture = idleTexture; 
+        jumpTexture = idleTexture;
     }
 
     // Set the initial texture and setup the sprite
@@ -37,20 +36,18 @@ void Player::loadAssets()
     sprite.setScale(3.f, 3.f);
 }
 
-
 // This handles "one-shot" inputs, like jumping
 void Player::handleEvents(sf::Event event)
 {
     if (event.type == sf::Event::KeyPressed)
     {
-        if ( (event.key.code == sf::Keyboard::Space || event.key.code == sf::Keyboard::Up) && onGround )
+        if ((event.key.code == sf::Keyboard::Space || event.key.code == sf::Keyboard::Up) && onGround)
         {
             velocity.y = -JUMP_SPEED;
             onGround = false;
         }
     }
 }
-
 
 void Player::update(float deltaTime)
 {
@@ -68,11 +65,16 @@ void Player::update(float deltaTime)
         sprite.setScale(-3.f, 3.f);
 
     // --- State Machine (Animation) ---
-    const sf::Texture* oldTexture = sprite.getTexture();
+    const sf::Texture *oldTexture = sprite.getTexture();
 
-    if (!onGround)
+    if (!onGround & velocity.x == 0.f)
     {
         sprite.setTexture(jumpTexture);
+        currentFrameCount = JUMP_FRAMES;
+    }
+    else if (!onGround & velocity.x != 0.f)
+    {
+        sprite.setTexture(walkTexture);
         currentFrameCount = JUMP_FRAMES;
     }
     else if (velocity.x != 0.f)
@@ -85,7 +87,6 @@ void Player::update(float deltaTime)
         sprite.setTexture(idleTexture);
         currentFrameCount = IDLE_FRAMES;
     }
-    
     // If we changed textures, reset the animation
     if (oldTexture != sprite.getTexture())
     {
@@ -119,25 +120,23 @@ void Player::update(float deltaTime)
     {
         animTimer = 0.f;
         animFrame++;
-        
+
         if (animFrame >= currentFrameCount)
         {
             animFrame = 0;
         }
     }
-    
+
     // --- Update the "cookie cutter" ---
     sprite.setTextureRect(sf::IntRect(
         animFrame * FRAME_WIDTH,
         0, // Row is 0 (as each texture is its own file)
         FRAME_WIDTH,
-        FRAME_HEIGHT
-    ));
+        FRAME_HEIGHT));
 }
 
-
 // This just draws the sprite to the window
-void Player::draw(sf::RenderWindow& window)
+void Player::draw(sf::RenderWindow &window)
 {
     window.draw(sprite);
 }
